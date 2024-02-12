eaedb352e16a8552347863e0913409780988d3bd
CiemnyWilk <117241918+CiemnyWilk@users.noreply.github.com>
2024-02-12 16:44:56 -0700

    Add ability triggering functionality to Ability_trigger.cs

--- Assets/Scripts/Abiilities/Ability_trigger.cs
+++ Assets/Scripts/Abiilities/Ability_trigger.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Ability_trigger : MonoBehaviour
 {
+    GameObject eventHandler;
+
+    //How to change what ability is given
+    [SerializeField] bool speedAbility;
+
+    void Start()
+    {
+        eventHandler = GameObject.Find("EventSystem");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Ability_Trigger_P1")
         {
             Debug.Log("P1 Ability Triggered");
-            //Add sending data to event system
+            eventHandler.gameObject.GetComponent<Abilites_Handler>().P1AbilityTriggered(1);
         }
         else if (other.tag == "Ability_Trigger_P2")
         {
