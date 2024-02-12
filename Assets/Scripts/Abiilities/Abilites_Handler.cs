d3edc598deb585a4f8b7cf5e60fc75488db2acf0
CiemnyWilk <117241918+CiemnyWilk@users.noreply.github.com>
2024-02-12 16:40:32 -0700

    Add P1AbilityTriggered and P2 method to Abilites_Handler.cs

--- Assets/Scripts/Abiilities/Abilites_Handler.cs
+++ Assets/Scripts/Abiilities/Abilites_Handler.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Abilites_Handler : MonoBehaviour
 {
-    
+    public void P1AbilityTriggered(int ability)
+    {
+        Debug.Log("P1 has used ability: " + ability);
+    }
+
+    public void P2AbilityTriggered(int ability)
+    {
+        Debug.Log("P2 has used ability: " + ability);
+    }
 }
