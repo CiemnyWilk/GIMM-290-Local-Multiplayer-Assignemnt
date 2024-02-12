081208606d21093701a70e79851f6f20574be37f
CiemnyWilk <117241918+CiemnyWilk@users.noreply.github.com>
2024-02-12 16:57:22 -0700

    Add player objects and implement P1 speed ability

--- Assets/Scripts/Abiilities/Abilites_Handler.cs
+++ Assets/Scripts/Abiilities/Abilites_Handler.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class Abilites_Handler : MonoBehaviour
 {
+    //Player Objects
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+
+    //Runs all the abilities for P1
     public void P1AbilityTriggered(int ability)
     {
         Debug.Log("P1 has used ability: " + ability);
+        if (ability == 0)
+        {
+            Debug.Log("P1 has used an unfinished ability");
+        }
+        else if (ability == 1)
+        {
+            Debug.Log("P1 has used speed ability");
+        }
+        // else if (ability == 2)
+        // {
+        //     Debug.Log("P1 has used jump ability");
+        // }
     }
 
+    //Runs all the abilities for P2
     public void P2AbilityTriggered(int ability)
     {
         Debug.Log("P2 has used ability: " + ability);
