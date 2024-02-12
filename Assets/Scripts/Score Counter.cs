19394b516eef26331c797f41c99df645d86821cd
CiemnyWilk <117241918+CiemnyWilk@users.noreply.github.com>
2024-02-12 12:31:26 -0700

    added tmp and scores

--- Assets/Scripts/Score Counter.cs
+++ Assets/Scripts/Score Counter.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreCounter : MonoBehaviour
 {
+    //Score Variables
     public int p1Score = 0;
     public int p2Score = 0;
 
+    //Score GameObjects
+    [SerializeField] private TMP_Text p1Text;
+    [SerializeField] private TMP_Text p2Text;
+
     public void FixedUpdate()
     {
-        Debug.Log("Player 1 Score: " + p1Score + " Player 2 Score: " + p2Score);    
+        //Debug.Log("Player 1 Score: " + p1Score + " Player 2 Score: " + p2Score);
+        
+        p1Text.SetText("P1 Score: " + p1Score);
+        p2Text.SetText("P2 Score: " + p2Score);
+
+        if (p1Score == 10 || p2Score == 10)
+        {
+            Debug.Log("Winner!");
+        }
     }
 }
