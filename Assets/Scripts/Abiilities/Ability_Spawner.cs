d7fa949f41d01c586b5923c2b6a3622640703a24
CiemnyWilk <117241918+CiemnyWilk@users.noreply.github.com>
2024-02-14 13:04:43 -0700

    Refactor code for improved performance

---
+++ Assets/Scripts/Abiilities/Ability_Spawner.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_Spawner : MonoBehaviour
+{
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
