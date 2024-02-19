457ea46d5cefb7c2ceb6cda6727f00159e48f67e
CiemnyWilk <117241918+CiemnyWilk@users.noreply.github.com>
2024-02-18 20:32:37 -0700

    Started first level and Spawner

--- Assets/Scripts/Abiilities/Ability_Spawner.cs
+++ Assets/Scripts/Abiilities/Ability_Spawner.cs
@@ -1,12 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ability_Spawner : MonoBehaviour
 {
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        
-    }
+    [SerializeField] float[] xChords;
+    [SerializeField] float[] yChords;
+    [SerializeField] float[] zChords;
 }
