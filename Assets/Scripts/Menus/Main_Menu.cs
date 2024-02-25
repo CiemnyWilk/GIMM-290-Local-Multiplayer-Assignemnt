0db1b547404bea87025df9267cea0f7c44e434ac
CiemnyWilk <117241918+CiemnyWilk@users.noreply.github.com>
2024-02-25 15:27:23 -0700

    Added working Main Menu with Level select for future Levels

--- Assets/Scripts/Menus/Main_Menu.cs
+++ Assets/Scripts/Menus/Main_Menu.cs
@@ -9,13 +9,30 @@ public class Main_Menu : MonoBehaviour
     [SerializeField] private GameObject optionsMenu;
     [SerializeField] private GameObject howToMenu;
     
-    
+    public void mainMenuActive()
+    {
+        // Set the main menu to active
+        mainMenu.SetActive(true);
+        // Set the level select menu to inactive
+        levelSelectMenu.SetActive(false);
+        // Set the options menu to inactive
+        optionsMenu.SetActive(false);
+        // Set the how to menu to inactive
+        howToMenu.SetActive(false);
+    }
+
     public void LevelSelect()
     {
         // Set the level select menu to active
         levelSelectMenu.SetActive(true);
         // Set the main menu to inactive
         mainMenu.SetActive(false);
+    }
+
+    public void Level1()
+    {
+        // Load the game scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level_1");
     }
 
     public void Options()
